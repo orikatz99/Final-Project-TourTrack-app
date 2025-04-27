69627b01a54f283f93fc0774cbf2b34c2d200a83
Shani <shanihlli1234@gmail.com>
2025-04-27 16:04:03 +0300

    Search XML - complete

--- app/src/main/java/com/example/myapplication/ui/search/NotificationsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/search/NotificationsFragment.java
@@ -4,26 +4,57 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentNotificationsBinding;
-
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-
+        // משתמשים ב-Binding בלבד
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // עכשיו חיבור הספינרים נעשה דרך binding
+        Spinner spi_region = binding.spiRegions;
+        Spinner spi_trail_catagories = binding.spiTrailCatagories;
+        Spinner spi_attractions = binding.spiAttractions;
+
+        // Adapter לאזורים
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.galil_tahton_regions,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spi_region.setAdapter(adapter);
+
+        // Adapter לאטרקציות
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.attraction_preferences,
+                android.R.layout.simple_spinner_item
+        );
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spi_attractions.setAdapter(adapter2);
+
+        // Adapter לקטגוריות מסלולים
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.trail_categories,
+                android.R.layout.simple_spinner_item
+        );
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spi_trail_catagories.setAdapter(adapter3);
 
         return root;
     }
@@ -34,3 +65,4 @@ public class NotificationsFragment extends Fragment {
         binding = null;
     }
 }
+
