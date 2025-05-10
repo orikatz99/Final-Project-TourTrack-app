1b374462475f41f07857a165b082cd63495c3a06
Ori Katz <orikatz99@walla.co.il>
2025-05-10 16:00:06 +0300

    check

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.activity)
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.android.flexbox:flexbox:3.0.0")
 
 
     testImplementation("junit:junit:4.13.2")
